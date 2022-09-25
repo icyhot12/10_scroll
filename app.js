ccfc5697027d78112bd34d0597b6d01cb53948b3
icyhot12 <rmierniczek1@gmail.com>
2022-09-25 17:15:35 +0200

    finish navbar toggle logic/styling

--- app.js
+++ app.js
@@ -7,9 +7,26 @@ siteYear.innerHTML = new Date().getFullYear();
 const navToggle = document.querySelector('.menu-toggle');
 const linksContainer = document.querySelector('.links-container');
 const links = document.querySelector('.links');
+const banner = document.querySelector('.banner')
 
 navToggle.addEventListener('click', () => {
     linksContainer.classList.toggle('show-links');
+    banner.classList.toggle('move-down')
+
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const linksHeight = links.getBoundingClientRect().height;
+
+    if (containerHeight === 0) {
+        linksContainer.style.height = `${linksHeight}px`
+    } else {
+        linksContainer.style.height = 0
+    }
+})
+
+//fixed navbar
+
+window.addEventListener('scroll', () => {
+    console.log(window.pageYOffset)
 })
 
 
