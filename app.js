62a3a8eb7ab3f37f0b8db9f194b5f97e94923500
icyhot12 <rmierniczek1@gmail.com>
2022-09-25 20:34:35 +0200

    add all logic, styles

--- app.js
+++ app.js
@@ -7,11 +7,9 @@ siteYear.innerHTML = new Date().getFullYear();
 const navToggle = document.querySelector('.menu-toggle');
 const linksContainer = document.querySelector('.links-container');
 const links = document.querySelector('.links');
-const banner = document.querySelector('.banner')
 
 navToggle.addEventListener('click', () => {
     linksContainer.classList.toggle('show-links');
-    banner.classList.toggle('move-down')
 
     const containerHeight = linksContainer.getBoundingClientRect().height;
     const linksHeight = links.getBoundingClientRect().height;
@@ -24,9 +22,46 @@ navToggle.addEventListener('click', () => {
 })
 
 //fixed navbar
+const navbar = document.getElementById('nav');
+const topLink = document.querySelector('.top-link')
 
 window.addEventListener('scroll', () => {
-    console.log(window.pageYOffset)
+    const scrollHeight = window.pageYOffset;
+    const navHeight = navbar.getBoundingClientRect().height
+
+    if (scrollHeight > navHeight) {
+        navbar.classList.add('fixed-nav')
+    } else {
+        navbar.classList.remove('fixed-nav')
+    }
+
+    if (scrollHeight > 500) {
+        topLink.classList.add('show-link')
+    } else {
+        topLink.classList.remove('show-link')
+    }
 })
 
+//scrolling links
 
+const scrollLinks = document.querySelectorAll('.scroll-link');
+
+scrollLinks.forEach((link) => {
+    link.addEventListener('click', (e) => {
+        // prevent default behaviour
+        e.preventDefault();
+        // navigato to specific id
+        const id = e.currentTarget.getAttribute('href').slice(1)
+        const element = document.getElementById(id);
+        // calculate heights
+        const navHeight = navbar.getBoundingClientRect().height
+        const containerHeight = linksContainer.getBoundingClientRect().height;
+        let position = element.offsetTop - navHeight;
+
+        window.scrollTo({
+            left:0,
+            top: position,
+        })
+        linksContainer.style.height = 0;
+    })
+})
